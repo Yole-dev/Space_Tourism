33cdba6d0199ddf0fe3ce6d2d9611037cd228dfb
Yole <igbavagodswill4@gmail.com>
2025-06-05 00:15:04 +0100

    resolved naming conflict affecting vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout";
-import HomePage from "./assets/pages/home";
+import HomePage from "./assets/pages/home.jsx";
 import Destination from "./assets/pages/Destination";
 import Crew from "./assets/pages/crew";
 import Technology from "./assets/pages/Technology";
@@ -14,7 +14,7 @@ export default function App() {
           path="/"
           element={
             <Layout>
-              <Home />
+              <HomePage />
             </Layout>
           }
         />
