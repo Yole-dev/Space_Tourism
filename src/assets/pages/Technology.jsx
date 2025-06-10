ee0c6bd76ed2ce784b284a06c9b88d675a8f2d66
Yole <igbavagodswill4@gmail.com>
2025-06-10 17:14:35 +0100

    finished creating the last page of the website, making it responsive and functional

--- src/assets/pages/Technology.jsx
+++ src/assets/pages/Technology.jsx
@@ -44,7 +44,7 @@ const techData = [
 
 export default function Technology() {
   const { width } = useScreenWidth();
-  const scope = width < 768 ? "portrait" : "landscape";
+  const scope = width < 768 || width > 1200 ? "portrait" : "landscape";
 
   const backgrounds = {
     mobile: mobileBackground,
@@ -75,20 +75,20 @@ function PageContent({ imgScope }) {
     "/src/assets/images/technology/image-space-capsule-portrait.jpg";
 
   return (
-    <section className=" w-full flex flex-col gap-[5rem]">
-      <p className="self-center flex items-center font-barlow-condensed font-[300] text-white text-[16px] tracking-[15%] uppercase gap-[1rem] ">
+    <section className=" w-full flex flex-col gap-[5rem] xl:w-[1275px] xl:self-end ">
+      <p className="self-center flex items-center font-barlow-condensed font-[300] text-white text-[16px] tracking-[15%] uppercase gap-[1rem] md:self-start md:ps-[2rem] md:text-[20px] xl:text-[28px] ">
         <span className=" font-[400] text-custom-fade">03</span>
         <span>space launch 101</span>
       </p>
 
-      <div className=" w-full flex flex-col items-center gap-[2rem] ">
-        <div className=" w-full h-[258px] overflow-hidden">
+      <div className=" w-full flex flex-col items-center gap-[2rem] xl:flex-row-reverse ">
+        <div className=" w-full h-[258px] overflow-hidden md:h-[357px] xl:w-[608px] xl:h-[734px] xl:flex xl:justify-end xl:items-center ">
           <img
             src={image}
             alt={currentTech.title}
             className={`w-full h-full object-cover ${
               image === capsuleImg ? "object-center" : "object-bottom"
-            } `}
+            } md:object-center xl:h-[600px] xl:w-[608px] `}
           />
         </div>
 
@@ -102,19 +102,20 @@ function PageContent({ imgScope }) {
 
 function TechnologyDetails({ children, tech }) {
   return (
-    <section className=" w-full flex flex-col items-center gap-[3rem] ">
+    <section className=" w-full flex flex-col items-center gap-[3rem] xl:w-[635px] xl:h-[304px] xl:flex-row xl:items-start xl:gap-[2rem] ">
       {children}
 
-      <div className=" w-full flex flex-col items-center gap-[0.5rem] text-center">
-        <p className="font-bellefair font-[300] text-[18px] text-custom-fade uppercase">
+      <div className=" w-[327px] flex flex-col items-center gap-[0.5rem] text-center md:w-[512px] xl:w-full xl:items-start xl:text-left ">
+        <p className="font-bellefair font-[300] text-[18px] text-custom-fade uppercase md:text-[24px] xl:text-[32px] ">
           the terminology...
         </p>
 
-        <p className=" font-bellefair text-[24px] uppercase "> {tech.title} </p>
+        <p className=" font-bellefair text-[24px] uppercase md:text-[40px] xl:text-[56px] ">
+          {tech.title}
+        </p>
 
-        <p className=" font-barlow font-[400] text-[15px] text-custom-fade leading-[180%] px-[0.5rem] ">
-          {" "}
-          {tech.details}{" "}
+        <p className=" font-barlow font-[400] text-[15px] text-custom-fade leading-[180%] md:text-[16px] xl:font-[300] xl:text-[18px] xl:text-left ">
+          {tech.details}
         </p>
       </div>
     </section>
@@ -123,14 +124,14 @@ function TechnologyDetails({ children, tech }) {
 
 function Nav({ activeTab, setActiveTab }) {
   return (
-    <ul className=" w-full flex justify-center items-center gap-[1.5rem] ">
+    <ul className=" w-full flex justify-center items-center gap-[1.5rem] xl:flex-col xl:w-[80px] ">
       {techData.map((tech, i) => (
         <li
-          className={` w-[40px] h-[40px] flex items-center justify-center font-bellefair text-[18px] text-white ${
+          className={` w-[40px] h-[40px] flex items-center justify-center font-bellefair text-[18px] ${
             activeTab === tech.id && "text-black"
-          } border-2 border-white rounded-full bg-transparent ${
-            activeTab === tech.id && "bg-white text-black"
-          }  `}
+          } border-2 border-custom-fade rounded-full bg-transparent ${
+            activeTab === tech.id && "bg-white"
+          } cursor-pointer md:w-[56px] md:h-[56px] md:text-[24px] xl:w-[80px] xl:h-[80px] xl:text-[32px] xl:border-[1px] `}
           key={tech.id}
           onClick={() => setActiveTab(tech.id)}
         >
